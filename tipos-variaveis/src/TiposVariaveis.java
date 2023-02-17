c7f4e335248f61429f8e18f16d144aa6f3475e9d
VitorBando <vitorm.804@gmail.com>
2023-02-17 14:18:49 -0300

    Finalizado o tema de tipos e variaveis.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Vitor Bando";
+
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero1 = 1;
+
+        numero1 = 2;
+
+        int numero2 = 5;
+
+        numero2 = 10;
+
+        System.out.println(numero2);
+
+        final double VALOR_DE_PI = 3.14;
+
+        //VALOR_DE_PI = 10.33;
+
+
+        
     }
 }
