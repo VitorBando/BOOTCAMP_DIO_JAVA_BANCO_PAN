ee0c2f142992d187e9b3939b5dcae8d09c060c4b
VitorBando <vitorm.804@gmail.com>
2023-02-19 17:14:55 -0300

    Conhecendo javadoc e suas funcionabilidades, juntamente com comentários e declarações.

---
+++ documentacao/src/Calculadora.java
@@ -0,0 +1,32 @@
+/*<h1>Calculadora</h1>
+* A Calculadora realiza operações  matemáticas  entre números  inteiros
+* <p>
+* <b>Note:</b> Leia atentamente  a documentação  desta classe para desfrutar dos recursos oferecidos pelo autor.
+* 
+* @author Vitor Bando
+* @version 1.0
+* @since 19/02/2023
+*/
+
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações  matemáticas  entre números  inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente  a documentação  desta classe para desfrutar dos recursos oferecidos pelo autor.
+ * @author Vitor Bando
+ * @version 1.0
+ * @since 19/02/2023
+ */
+
+public class Calculadora {
+
+    /**
+     * Este método é utilizado para  somar dois números inteiros.
+     * @param numeroUm este é o primeiro número para o método
+     * @param numeroDois este é o segundo número para o método
+     * @return int o resultado  deste método é a soma  dos dois números
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
---
+++ documentacao/src/Comentarios.java
@@ -0,0 +1,37 @@
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá eu sou um comentário e uma única linha
+
+        /*
+         * Eu sou um comentário 
+         * que posso ser detalhado
+         * quando necessário
+         */
+
+    }
+
+
+    /*
+     * Esse método foi feito com pressa
+     * por isso foi abreviado o nome das
+     * variaveis, ele tem a finalidade de 
+     * somar ou multiplicar dois números
+     */
+    public int somaMultiplica (int n, int x, String m){
+        int r = 0;
+        if (m == "M") {
+            r = n * x;            
+        } else {
+            r = n + x;
+        }
+        return r;
+    }
+    /**
+     * Essas duas estrelinhas consideram uma documentação da linguagem
+     */
+    public void metodo(){
+
+    }
+}
