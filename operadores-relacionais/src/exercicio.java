7c059c027b71adcb326cf126b7a0d59ec315be40
VitorBando <vitorm.804@gmail.com>
2023-02-22 09:02:45 -0300

    Aprendendo sobre operadores relacionais com exercicio.

---
+++ operadores-relacionais/src/exercicio.java
@@ -0,0 +1,44 @@
+
+public class exercicio {
+    public static void main(String[] args) {
+        
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 2.5f;
+        float f2 = 3.9f;
+
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Vitor";
+        String s2 = "Bando";
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+
+        System.out.println("i1 == i2" + (i1 == i2));
+        System.out.println("i1 != i2" + (i1 != i2));
+        System.out.println("i1 >= i2" + (i1 >= i2));
+        System.out.println("i1 <= i2" + (i1 <= i2));
+
+        System.out.println("f1 == f2" + (f1 == f2));
+        System.out.println("f1 != f2" + (f1 != f2));
+        System.out.println("f1 >= f2" + (f1 >= f2));
+        System.out.println("f1 <= f2" + (f1 <= f2));
+
+        System.out.println("c1 == c2" + (c1 == c2));
+        System.out.println("c1 != c2" + (c1 != c2));
+        System.out.println("c1 >= c2" + (c1 >= c2));
+        System.out.println("c1 <= c2" + (c1 <= c2));
+
+        System.out.println("s1 == s2" + (s1 == s2));
+        System.out.println("s1 != s2" + (s1 != s2));
+        //System.out.println("s1 >= s2" + (s1 >= s2));
+        //System.out.println("s1 <= s2" + (s1 <= s2));
+
+        System.out.println("b1 == b2" + (b1 == b2));
+        System.out.println("b1 != b2" + (b1 != b2));
+        //System.out.println("b1 >= b2" + (b1 >= b2));
+        //System.out.println("b1 <= b2" + (b1 <= b2));
+    }
+}
