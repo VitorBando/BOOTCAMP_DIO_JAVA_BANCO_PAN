20cda30e02c967339d0ed2e24453ebce99878b5d
VitorBando <vitorm.804@gmail.com>
2023-03-05 12:54:22 -0300

    Aprimorando conhecimentos em Estrutura de repetição e arrays

---
+++ estrutura-de-repeticao-e-array/estrutura-de-repeticao-e-array/src/loops/Ex1_NomeIdade.java
@@ -0,0 +1,32 @@
+package loops;
+
+import java.util.Scanner;
+
+/**
+ * @author Vitor Bando
+ * @version 19.0.1
+ * @since 03/03/2023
+ */
+
+public class Ex1_NomeIdade {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        String nome;
+        int idade;
+
+        while (true) {
+            
+            System.out.println("Digite seu nome:"  );
+            nome = scan.next();
+            if (nome.equals("0")) break;
+            System.out.println("Digite seu idade:"  );
+            idade = scan.nextInt();
+        }
+
+        System.out.println("Continua aqui...");
+
+        scan.close();
+    }
+}
